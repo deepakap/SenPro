4b87f64899f326f2bb0636eeb852826feafb2e00
Deepak <deepak_ap007@yahoo.co.in>
2017-10-05 11:38:40 -0700

    All commit

---
+++ src/test/java/com/app/bot/senbot/AppTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.app.bot.senbot;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author deepakkumar
+ *
+ */
+public class AppTest {
+
+	@Test
+	private void printmessage() {
+	
+		System.out.println( " The test case !");
+	}
+	
+}
