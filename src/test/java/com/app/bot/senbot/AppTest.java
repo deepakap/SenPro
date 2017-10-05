0f91b05a793fd2893781a6ff22d8fdd2105c03f9
Deepak <deepak_ap007@yahoo.co.in>
2017-10-05 11:58:10 -0700

    Update AppTest.java

--- src/test/java/com/app/bot/senbot/AppTest.java
+++ src/test/java/com/app/bot/senbot/AppTest.java
@@ -6,12 +6,12 @@ package com.app.bot.senbot;
 import org.testng.annotations.Test;
 
 /**
- * @author deepakkumar
+ * @author deepakkumar A Patil
  *
  */
 public class AppTest {
 
-	@Test
+	@Test(enabled=true)
 	private void printmessage() {
 	
 		System.out.println( " The test case !");
